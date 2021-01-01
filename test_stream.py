deb2743dbc8128b70f4e86c6aa06873d2eaec7a6
gitwetguy <g0930421313@gmail.com>
2021-01-02 02:30:54 +0800

    RDD data input

--- test_stream.py
+++ test_stream.py
@@ -1,9 +1,25 @@
+import sys
+from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-ssc = StreamingContext(sc, 1)
-lines = ssc.textFileStream("file:///home/master/spark_test/Spark_test")
-words = lines.flatMap(lambda line: line.split(" "))
-pairs = words.map(lambda word: (word, 1))
-wordCounts = pairs.reduceByKey(lambda x, y: x + y)
-wordCounts.pprint()
-ssc.start()
-ssc.awaitTermination()
+
+"""
+This is use for create streaming of text from txt files that creating dynamically 
+from files.py code. This spark streaming will execute in each 3 seconds and It'll
+show number of words count from each files dynamically
+"""
+
+
+def main():
+    sc = SparkContext(appName="StreamingcountByWindow");
+    ssc = StreamingContext(sc, 3)   #Streaming will execute in each 3 seconds
+    lines = ssc.textFileStream('./log/')  #'log/ mean directory name
+    counts = lines.flatMap(lambda line: line.split(" ")) \
+        .map(lambda x: (x, 1)) \
+        .reduceByKey(lambda a, b: a + b)
+    counts.pprint()
+    ssc.start()
+    ssc.awaitTermination()
+
+
+if __name__ == "__main__":
+    main()
